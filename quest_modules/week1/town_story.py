868a7fd678309d961a532cc559a328e14a632c18
Matvey Varenitsa <subwayminder@gmail.com>
2024-03-20 03:43:17 +0700

    fix town story

--- quest_modules/week1/town_story.py
+++ quest_modules/week1/town_story.py
@@ -8,7 +8,8 @@ from utils.gas_checker import check_gas
 from utils.helpers import retry
 from eth_account.messages import encode_defunct
 import requests
-from datetime import datetime, timedelta
+import sys
+import time
 
 
 class TownStory(Account):
@@ -17,7 +18,7 @@ class TownStory(Account):
         self.contract = self.get_contract(TOWN_STORY_CONTRACT, TOWN_STORY_ABI)
     
     def getSign(self):
-        nonce = 19799
+        nonce = int(int(time.time()) / 86400)
         address = str(self.address).lower()
         address = address[0:19] + '...' + address[24:]
         msgText = 'Welcome to Town Story! \n\nClick to sign in and accept the Town Story\nTerms of Service:\nhttps://townstory.io/\n\nThis request will not trigger a blockchain\ntransaction or cost any gas fees.\n\nYour authentication status will reset after\neach session.\n\nWallet address:\n'
@@ -26,6 +27,8 @@ class TownStory(Account):
         sign = self.w3.eth.account.sign_message(message, private_key=self.private_key).signature
         return sign
 
+    # @retry
+    # @check_gas
     async def signUp(self):
         logger.info(f"[{self.account_id}][{self.address}] Регистрация в Town Story")
         sign = self.getSign().hex()
@@ -48,12 +51,16 @@ class TownStory(Account):
         })
         loginResponse = loginRequest.json()
         if('signature' in loginResponse['response']):
-            signForSignUp = loginRequest['response']['signature']
-            deadline = loginRequest['response']['deadline']
+            signForSignUp = loginResponse['response']['signature']
+            deadline = loginResponse['response']['deadline']
             txData = await self.getTxData()
             tx = await self.contract.functions.createAccountSign(signForSignUp, 0, int(deadline)).build_transaction(txData)
             signedTx = await self.sign(tx)
             txHash = await self.send_raw_transaction(signedTx)
             await self.wait_until_tx_finished(txHash.hex())
-        else:
+        elif('sig' in loginResponse['response']):
             logger.info(f"[{self.account_id}][{self.address}] Регистрация в Town Story - уже зарегистрирован")
+        elif('failed' in loginResponse):
+            logger.error(f"[{self.account_id}][{self.address}] Регистрация в Town Story - ошибка регистрации")
+        else:
+            logger.error(f"[{self.account_id}][{self.address}] Регистрация в Town Story - ошибка регистрации")
